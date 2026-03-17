d15a4822fbf81ef94fbc368f2d9e7dd0d0cc5dbe
Temirlan Bayangazy <bayangazyt@gmail.com>
2026-03-17 14:56:23 +0500

    feat: add ZapOptions implementation for structured logging with context support

---
+++ internal/infrastructure/logger/zap_options.go
@@ -0,0 +1,38 @@
+package logger
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+type loggerCtxKeyType struct{}
+
+var loggerCtxKey = &loggerCtxKeyType{}
+
+func AddZapField(ctx context.Context, field zap.Field) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	options, ok := ctx.Value(loggerCtxKey).([]zap.Field)
+	if !ok {
+		options = []zap.Field{}
+	}
+
+	options = append(options, field)
+	return context.WithValue(ctx, loggerCtxKey, options)
+}
+
+func WithCtxData(ctx context.Context, log *zap.Logger) *zap.Logger {
+	if ctx == nil {
+		return log
+	}
+
+	options, ok := ctx.Value(loggerCtxKey).([]zap.Field)
+	if ok {
+		log = log.With(options...)
+	}
+
+	return log
+}
